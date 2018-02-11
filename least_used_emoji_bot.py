f6f5e9c65a506d0cc2f9d15030587d09d599ce82
Jeremy Schmidt <jpschmid@ncsu.edu>
2018-02-11 18:56:56 -0500

    Add tracking system to report the time an emoji has been the least used

--- least_used_emoji_bot.py
+++ least_used_emoji_bot.py
@@ -1,5 +1,8 @@
+import math
 import requests, json, tweepy
 from credentials import *
+from datetime import datetime
+import os.path
 
 def get_least_used_emoji():
     url = "http://emojitracker.com/api/rankings"
@@ -28,6 +31,25 @@ def get_emoji_char(jsonData):
 def get_emoji_name(jsonData):
     return jsonData['name'].capitalize()
 
+def compare_results(emojiName, emojiChar):
+    resultsPath = "results.txt"
+    if (os.path.exists(resultsPath)):
+        with open("results.txt", "r") as f:
+            current, initialTime = f.readline().split(" ")
+            initialTime = datetime.fromtimestamp(int(initialTime))
+        if (current == emojiChar):
+            timeStanding = datetime.utcnow() - initialTime
+            daysStanding = timeStanding.days
+            if daysStanding > 1:
+                return "%s (%s) has been the least used emoji for %i days" %(emojiChar, emojiName, daysStanding)
+        else:
+            with open("results.txt", 'w') as f:
+                resultsText = str(emojiChar) + " " + str(math.floor(datetime.utcnow().timestamp()))
+                f.write(resultsText)
+            return "The least used emoji is now: %s (%s)" % (leastUsedEmojiChar, leastUsedEmojiName)
+
+    return "The least used emoji is currently: %s (%s)" % (leastUsedEmojiChar, leastUsedEmojiName)
+
 
 # Access and authorize our Twitter credentials from credentials.py
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
@@ -37,6 +59,10 @@ api = tweepy.API(auth)
 leastUsedEmojiData = get_least_used_emoji()
 leastUsedEmojiChar = get_emoji_char(leastUsedEmojiData)
 leastUsedEmojiName = get_emoji_name(leastUsedEmojiData)
+
+
+tweetText = compare_results(leastUsedEmojiName, leastUsedEmojiChar)
+
 
 update_profile_image(leastUsedEmojiData)
 
