f57750d80880cbbeeb5f90950a5eeec0e18bb55b
Jeremy Schmidt <jpschmid@ncsu.edu>
2018-02-11 20:21:46 -0500

    fix filename typo

--- least_used_emoji_bot.py
+++ least_used_emoji_bot.py
@@ -34,7 +34,7 @@ def get_emoji_name(jsonData):
 def compare_results(emojiName, emojiChar):
     resultsPath = "results.txt"
     if (os.path.exists(resultsPath)):
-        with io.open("test", mode="r", encoding="utf-8") as f:
+        with io.open("results.txt", mode="r", encoding="utf-8") as f:
             current, initialTime = f.readline().split(" ")
             initialTime = datetime.fromtimestamp(int(initialTime))
         if (current == emojiChar):
